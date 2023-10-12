d6eedf8c696c97b9411b831a6bd40a531956a79f
ilya <ilya.antropov2018@mail.ru>
2023-10-12 23:40:15 +0700

    We have defined the class `Uzel` which,represents a node in a Huffman tree.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Uzel {
+    public:
+            int key;
+            char s;
+            Uzel* left, + right;
+
+            Uzel() {
+                left = NULL;
+                right = NULL;
+            }
+
+            Uzel(Uzel* l, Uzel* r) {
+                left = l;
+                right = r;
+                key = l->key + r->key;
+            }
+
+            ~Uzel() {
+                while (left)
+                    delete left;
+                while (right)
+                    delete right;
+                left = NULL;
+                right = NULL;
+            }
+};
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
