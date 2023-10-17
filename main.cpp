8bafca235583cfe57955e12bf498165126eb313b
ilya <ilya.antropov2018@mail.ru>
2023-10-17 23:46:45 +0700

    We use the buildForDecode function to build a Huffman tree based on the character frequency information from the input file. It accepts an ifstream that contains information about the character rate and the encoded message. The function first clears the current tree if it already exists, and also clears m, code and buf to store information about the characters and their codes. The function then reads the character frequency information from the file and stores them in m. Next, a similar process of building a Huffman tree for encoding takes place, as described in a past commit The decode function is used to decode data from the input file and write it to the output file. It first calls the buildForDecode function to build a Huffman tree based on the information from the input file. The function then traverses the bits of the input file and the constructed Huffman tree. It first selects either the left or right subtree and continues traversing until a leaf node is reached, then the symbol that corresponds to the leaf node is written to the output file. The process is then repeated again from the root of the tree. The byte variable is used to read a byte from the file, and the count variable keeps track of the current position in the byte. When all bits of the byte have been processed, the next byte is read from the file and the process is repeated. The function returns true if decoding was successful and false if the input file was not opened.

--- main.cpp
+++ main.cpp
@@ -42,6 +42,8 @@ public:
     void buildForEncode(ifstream& f);
     double encode(ifstream& f, ofstream& g);
     void TreeHaf(Uzel* r);
+    void buildForDecode(ifstream& fg);
+    bool decode(ifstream& fg,ofstream& gf);
     struct Sort {
         bool operator() (const Uzel* l, const Uzel* r) {
             return l->key < r->key;
@@ -146,6 +148,77 @@ void Hafman::TreeHaf(Uzel* r) {
         code.pop_back();
 }
 
+void Hafman::buildForDecode(ifstream& f) {
+    if (root != NULL){
+        delete root;
+        root = NULL;
+    }
+    if (m.size() != 0)
+        m.clear();
+    if (code.size() != 0)
+        code.clear();
+    if (buf.size() != 0)
+        buf.clear();
+
+    int x1,x2;
+    char s;
+    f.read((char*)&x1, sizeof (x1));
+    while (x1 > 0) {
+        f.read((char*)&s, sizeof(s));
+        f.read((char*)&x2, sizeof(x2));
+        x1 -= 40;
+        m[s] = x2;
+    }
+    list<Uzel*> L;
+    for (ii = m.begin(); ii != m.end(); ii++) {
+        Uzel* p = new Uzel;
+        p->s = ii->first;
+        p->key = ii->second;
+        L.push_back(p);
+    }
+    while (L.size() != 1) {
+        L.sort(Sort());
+        Uzel* Left = L.front();
+        L.pop_front();
+        Uzel* Right = L.front();
+        L.pop_front();
+        Uzel* pr = new Uzel(Left, Right);
+        L.push_back(pr);
+    }
+    root = L.front();
+}
+
+bool Hafman::decode(ifstream& fg, ofstream& gf){
+    if (fg.is_open()) {
+        buildForDecode(fg);
+    }
+    else
+        return false;
+    TreeHaf(root);
+
+    char byte;
+    int count = 0;
+    Uzel* p = root;
+    byte = fg.get();
+    while (!fg.eof()) {
+        bool b = byte & (1 << (7 - count));
+        if (b)
+            p = p->right;
+        else
+            p = p->left;
+        if (p->right == NULL && p->left == NULL) {
+            gf << p->s;
+            p = root;
+        }
+        count++;
+        if (count == 8) {
+            count = 0;
+            byte = fg.get();
+        }
+    }
+    return true;
+}
+
 int main() {
     ifstream f("text.txt", ios::in | ios::binary);
     ofstream g("code.txt", ios::out | ios::binary);
@@ -153,5 +226,13 @@ int main() {
     double coef = haf.encode(f, g);
     std::cout << coef << std::endl;
     ifstream fg("code.txt", ios::out | ios::binary);
+    ofstream gf("output.txt", ios::out | ios::binary);
+    Hafman decodeHaf;
+    if (decodeHaf.decode(fg,gf))
+        std::cout << "decode" << std::endl;
+    else
+        std::cout << "didnt decode" << std::endl;
+    fg.close();
+    gf.close();
     return 0;
 }
